9421452efd8c389a02180ce4afa2ab0efc6e7fbc
Derek Rushforth <derekrushforth@gmail.com>
2019-05-29 09:19:50 -0700

    Gruntfile formatting

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,10 +1,9 @@
 module.exports = function(grunt) {
-
   // Include modules
-  require('load-grunt-tasks')(grunt, {pattern: ['grunt-*', 'assemble']});
-  require('time-grunt')(grunt);
+  require('load-grunt-tasks')(grunt, { pattern: ['grunt-*', 'assemble'] })
+  require('time-grunt')(grunt)
 
-  grunt.loadNpmTasks('grunt-ftp-deploy');
+  grunt.loadNpmTasks('grunt-ftp-deploy')
 
   var path = {
     css_src: 'src/stylesheets/global.scss',
@@ -17,8 +16,8 @@ module.exports = function(grunt) {
     dist_test_html_glob: 'dist_test/*.html',
     layouts: 'src/layouts',
     partials: 'src/partials/*',
-    images_src: 'src/images'
-  };
+    images_src: 'src/images',
+  }
 
   /* Configuration
   ================================================= */
@@ -30,8 +29,11 @@ module.exports = function(grunt) {
        common changes centralized to key files instead of being littered
        throughout the Gruntfile. This also makes it easy to .gitignore secrets
     ================================================= */
-    secret: grunt.file.exists('secrets.json') ? grunt.file.readJSON('secrets.json') : {},
+    secret: grunt.file.exists('secrets.json')
+      ? grunt.file.readJSON('secrets.json')
+      : {},
     config: grunt.file.readJSON('config.json'),
+    templates: grunt.file.readJSON('templates.json'),
 
     /* SASS
     ------------------------------------------------- */
@@ -39,24 +41,22 @@ module.exports = function(grunt) {
     sass: {
       styles: {
         src: path.css_src,
-        dest: path.css_dest
-      }
+        dest: path.css_dest,
+      },
     },
-
 
     /* Autoprefixer
     ------------------------------------------------- */
 
     autoprefixer: {
       options: {
-        browsers: ['last 6 versions', 'ie >= 9']
+        browsers: ['last 6 versions', 'ie >= 9'],
       },
       styles: {
         src: path.css_dest,
-        dest: path.css_dest
-      }
+        dest: path.css_dest,
+      },
     },
-
 
     /* Assemble
        Assembles our handlebars templates. Documentation might be incompatible since the latest version uses an entirely different build system.
@@ -68,37 +68,37 @@ module.exports = function(grunt) {
         layoutdir: path.layouts,
         partials: path.partials,
         flatten: true,
-        sender_name: "<%= config.strings.sender_name %>",
-        product_name: "<%= config.strings.product_name %>",
-        product_url: "<%= config.strings.product_url %>",
-        credit_card_statement_name: "<%= config.strings.credit_card_statement_name %>",
-        formal_company_name: "<%= config.strings.formal_company_name %>",
-        address_line_1: "<%= config.strings.address_line_1 %>",
-        address_line_2: "<%= config.strings.address_line_2 %>",
-        city: "<%= config.strings.city %>",
-        state: "<%= config.strings.state %>",
-        country: "<%= config.strings.country %>",
-        phone: "<%= config.strings.phone %>",
-        images_url: "<%= config.images.images_url %>",
-        use_images: "<%= config.images.use_images %>",
-        logo_file: "<%= config.images.logo_file %>",
-        logo_width: "<%= config.images.logo_width %>",
-        use_social_circles: "<%= config.images.use_social_circles %>",
-        twitter_url: "<%= config.images.twitter_url %>",
-        facebook_url: "<%= config.images.facebook_url %>",
-        pinterest_url: "<%= config.images.pinterest_url %>",
-        instagram_url: "<%= config.images.instagram_url %>",
-        dribbble_url: "<%= config.images.dribbble_url %>",
-        google_plus_url: "<%= config.images.google_plus_url %>",
-        youtube_url: "<%= config.images.youtube_url %>",
-        linkedin_url: "<%= config.images.linkedin_url %>"
+        sender_name: '<%= config.strings.sender_name %>',
+        product_name: '<%= config.strings.product_name %>',
+        product_url: '<%= config.strings.product_url %>',
+        credit_card_statement_name:
+          '<%= config.strings.credit_card_statement_name %>',
+        formal_company_name: '<%= config.strings.formal_company_name %>',
+        address_line_1: '<%= config.strings.address_line_1 %>',
+        address_line_2: '<%= config.strings.address_line_2 %>',
+        city: '<%= config.strings.city %>',
+        state: '<%= config.strings.state %>',
+        country: '<%= config.strings.country %>',
+        phone: '<%= config.strings.phone %>',
+        images_url: '<%= config.images.images_url %>',
+        use_images: '<%= config.images.use_images %>',
+        logo_file: '<%= config.images.logo_file %>',
+        logo_width: '<%= config.images.logo_width %>',
+        use_social_circles: '<%= config.images.use_social_circles %>',
+        twitter_url: '<%= config.images.twitter_url %>',
+        facebook_url: '<%= config.images.facebook_url %>',
+        pinterest_url: '<%= config.images.pinterest_url %>',
+        instagram_url: '<%= config.images.instagram_url %>',
+        dribbble_url: '<%= config.images.dribbble_url %>',
+        google_plus_url: '<%= config.images.google_plus_url %>',
+        youtube_url: '<%= config.images.youtube_url %>',
+        linkedin_url: '<%= config.images.linkedin_url %>',
       },
       pages: {
         src: [path.email_src],
-        dest: path.dist
-      }
+        dest: path.dist,
+      },
     },
-
 
     /* Inline
        Brings external resources in the HTML files. Basically just takes our CSS file and inserts it as a style block.
@@ -111,10 +111,9 @@ module.exports = function(grunt) {
         cwd: path.dist,
         ext: '.html',
         src: ['*.html'],
-        dest: path.dist
-      }
+        dest: path.dist,
+      },
     },
-
 
     /* Prettify
        Formats our template HTML files.
@@ -123,17 +122,16 @@ module.exports = function(grunt) {
 
     prettify: {
       options: {
-        indent: '2'
+        indent: '2',
       },
       html: {
         expand: true,
         cwd: path.dist,
         ext: '.html',
         src: ['*.html'],
-        dest: path.dist
-      }
+        dest: path.dist,
+      },
     },
-
 
     /* Premailer
        Adds all sorts of email magic. Adds inline styles to each element based off class. Also generates plain text versions and allows us to control commonly used table attributes via CSS.
@@ -142,47 +140,52 @@ module.exports = function(grunt) {
 
     premailer: {
       options: {
-        warnLevel: 'none'
+        warnLevel: 'none',
       },
       html: {
         options: {
-          removeComments: true
+          removeComments: true,
         },
-        files: [{
-          expand: true,
-          src: [path.dist_test_html_glob],
-          dest: ''
-        }]
+        files: [
+          {
+            expand: true,
+            src: [path.dist_test_html_glob],
+            dest: '',
+          },
+        ],
       },
       txt: {
         options: {
           mode: 'txt',
-          lineLength: 16384
+          lineLength: 16384,
         },
-        files: [{
-          expand: true,
-          src: [path.dist_html_glob],
-          dest: '',
-          ext: '.txt'
-        }]
-      }
+        files: [
+          {
+            expand: true,
+            src: [path.dist_html_glob],
+            dest: '',
+            ext: '.txt',
+          },
+        ],
+      },
     },
-
 
     'ftp-deploy': {
       build: {
         auth: {
-          host: "<%= config.ftp.host %>",
-          port: "<%= config.ftp.port %>",
-          username: "<%= secret.ftp.username %>",
-          password: "<%= secret.ftp.password %>"
+          host: '<%= config.ftp.host %>',
+          port: '<%= config.ftp.port %>',
+          username: '<%= secret.ftp.username %>',
+          password: '<%= secret.ftp.password %>',
         },
-        src: "<%= config.ftp.src %>",
-        dest: "<%= config.ftp.dest %>",
-        exclusions: ['path/to/source/folder/**/.DS_Store', 'path/to/source/folder/**/Thumbs.db']
-      }
+        src: '<%= config.ftp.src %>',
+        dest: '<%= config.ftp.dest %>',
+        exclusions: [
+          'path/to/source/folder/**/.DS_Store',
+          'path/to/source/folder/**/Thumbs.db',
+        ],
+      },
     },
-
 
     /* S3
        Pushes image assets to an Amazon S3 bucket.
@@ -191,18 +194,17 @@ module.exports = function(grunt) {
 
     s3: {
       options: {
-        accessKeyId: "<%= secret.s3.access_key_id %>",
-        secretAccessKey: "<%= secret.s3.secret_access_key %>",
-        bucket: "<%= config.s3.bucket %>",
-        region: "<%= config.s3.region %>",
-        overwrite: "<%= config.s3.overwrite %>"
+        accessKeyId: '<%= secret.s3.access_key_id %>',
+        secretAccessKey: '<%= secret.s3.secret_access_key %>',
+        bucket: '<%= config.s3.bucket %>',
+        region: '<%= config.s3.region %>',
+        overwrite: '<%= config.s3.overwrite %>',
       },
       build: {
         cwd: path.images_src,
-        src: "**"
-      }
+        src: '**',
+      },
     },
-
 
     /* Watch
        Watches all files in the src directory for changes.
@@ -210,16 +212,14 @@ module.exports = function(grunt) {
 
     watch: {
       options: {
-        livereload: true
+        livereload: true,
       },
       files: ['src/**/*'],
-      tasks: ['default']
+      tasks: ['default'],
     },
-
 
     /* Replace
     ------------------------------------------------- */
-
 
     replace: {
       // Premailer escapes URLs, so our mustachio variables for URLs get fully escaped, and the
@@ -232,8 +232,8 @@ module.exports = function(grunt) {
           { from: '%7D%7D', to: ' }}' },
           { from: '%7D%7D%22', to: ' }}' },
           { from: '%20}}', to: ' }}' },
-          { from: '{{%20', to: '{{ ' }
-        ]
+          { from: '{{%20', to: '{{ ' },
+        ],
       },
       // Add some additional attributes that grunt inline removed
       styleBlock: {
@@ -242,12 +242,11 @@ module.exports = function(grunt) {
         replacements: [
           {
             from: '<style>',
-            to: '<style type="text/css" rel="stylesheet" media="all">'
-          }
-        ]
-      }
+            to: '<style type="text/css" rel="stylesheet" media="all">',
+          },
+        ],
+      },
     },
-
 
     /* Copy
        This is mainly used to copy compiled templates to a new directory for the CSS to be inlined before being tested.
@@ -256,15 +255,16 @@ module.exports = function(grunt) {
 
     copy: {
       testTemplates: {
-        files: [{
-          expand: true,
-          src: [path.dist_html_glob],
-          dest: path.dist_test,
-          flatten: true
-        }]
-      }
+        files: [
+          {
+            expand: true,
+            src: [path.dist_html_glob],
+            dest: path.dist_test,
+            flatten: true,
+          },
+        ],
+      },
     },
-
 
     /* Spamcheck
        Sends all of our HTML files through Postmark’s spamcheck API.
@@ -273,10 +273,9 @@ module.exports = function(grunt) {
 
     spamcheck: {
       emails: {
-        src: [path.dist_html_glob]
-      }
+        src: [path.dist_html_glob],
+      },
     },
-
 
     /* Postmark
        Sends test emails through Postmark. Add and remove template targets as needed.
@@ -286,45 +285,73 @@ module.exports = function(grunt) {
 
     postmark: {
       options: {
-        serverToken: "<%= secret.postmark.server_token %>",
-        from: "<%= config.postmark.from %>",
-        to: "<%= config.postmark.to %>",
-        subject: "<%= config.postmark.subject %>",
+        serverToken: '<%= secret.postmark.server_token %>',
+        from: '<%= config.postmark.from %>',
+        to: '<%= config.postmark.to %>',
+        subject: '<%= config.postmark.subject %>',
       },
       // run "grunt postmark:welcome" - Sends just the welcome email
       welcome: {
-        src: 'dist_test/welcome.html'
+        src: 'dist_test/welcome.html',
       },
       // run "npm run flood" - Sends all of the emails. Be careful not to spam PM if you have a bunch of emails.
       flood: {
-        src: [path.dist_test_html_glob]
+        src: [path.dist_test_html_glob],
       },
       // run "npm run litmus" - Add a litmus test address here.
       litmus: {
-        to: "<%= config.strings.litmus_email %>",
-        src: 'dist_test/user_invitation.html'
-      }
-    }
+        to: '<%= config.strings.litmus_email %>',
+        src: 'dist_test/user_invitation.html',
+      },
+    },
 
-  });
+    // Push templates from your local file system to Postmark
+    postmarkPushTemplates: {
+      options: {
+        serverToken: '<%= secret.postmark.server_token %>',
+      },
+      your_server: {
+        templates: '<%= templates %>',
+      },
+    },
 
+    // Download your templates from Postmark and generate a boilerplate config.
+    // The config can be used with the "postmarkPushTemplates" task
+    postmarkSetupTemplates: {
+      options: {
+        serverToken: '<%= secret.postmark.server_token %>',
+        configOutputFile: 'templates.json',
+        templateOutputDest: 'dist',
+      },
+    },
+  })
 
   /* Tasks
   ================================================= */
 
-  grunt.registerTask('default', ['css', 'html']);
+  grunt.registerTask('default', ['css', 'html'])
 
   // Assets
-  grunt.registerTask('html', ['assemble', 'inline', 'premailer:txt', 'replace', 'prettify']);
-  grunt.registerTask('css', ['sass', 'autoprefixer']);
-  grunt.registerTask('images-ftp', ['ftp-deploy']);
-  grunt.registerTask('images-s3', ['s3']);
+  grunt.registerTask('html', [
+    'assemble',
+    'inline',
+    'premailer:txt',
+    'replace',
+    'prettify',
+  ])
+  grunt.registerTask('css', ['sass', 'autoprefixer'])
+  grunt.registerTask('images-ftp', ['ftp-deploy'])
+  grunt.registerTask('images-s3', ['s3'])
 
   // Testing
-  grunt.registerTask('spam', ['spamcheck']);
-  grunt.registerTask('litmus', ['testBuild', 'postmark:litmus']);
-  grunt.registerTask('flood', ['testBuild', 'postmark:flood']);
+  grunt.registerTask('spam', ['spamcheck'])
+  grunt.registerTask('litmus', ['testBuild', 'postmark:litmus'])
+  grunt.registerTask('flood', ['testBuild', 'postmark:flood'])
 
   // Before sending tests via Postmark, ensure that test builds with inlined CSS are generated
-  grunt.registerTask('testBuild', ['default', 'copy:testTemplates', 'premailer:html']);
-};
+  grunt.registerTask('testBuild', [
+    'default',
+    'copy:testTemplates',
+    'premailer:html',
+  ])
+}
